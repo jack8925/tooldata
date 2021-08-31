2e9f12f30c1964828f9c3a031396e1bb15c96417
jack8925 <39871543+jack8925@users.noreply.github.com>
2021-08-31 19:54:38 +0800

    Add files via upload

---
+++ OpenPictureMode.bat
@@ -0,0 +1,7 @@
+@echo off
+echo 新增".jpg"、".png"、".gif"的開啟方式
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".jpg" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff"
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".png" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff"
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".gif" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff"
+echo done
+pause
