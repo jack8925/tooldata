20a30622d49af98ddce4d9a8387fd9e31ff0dc99
jack8925 <39871543+jack8925@users.noreply.github.com>
2021-08-31 20:06:01 +0800

    Add files via upload

--- OpenPictureMode.bat
+++ OpenPictureMode.bat
@@ -1,4 +1,6 @@
-@echo off
+@ECHO OFF
+cd /d "%~dp0"&&fltmc>nul||mshta vbscript:CreateObject("Shell.Application").ShellExecute("%~s0","","","runas",1)(window.close)&&exit
+
 reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".jpg" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".png" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".gif" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" /f
