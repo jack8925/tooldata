8dbc39abbc3c8b2458f08aed3dd25cf922028117
jack8925 <39871543+jack8925@users.noreply.github.com>
2021-08-31 19:39:55 +0800

    Create OpenPictureMode.bat

---
+++ OpenPictureMode.bat
@@ -0,0 +1,6 @@
+@echo off
+echo 新增".jpg"、".png"、".png"的開啟方式
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".jpg" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff"
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".png" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff"
+reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".png" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff"
+echo done
